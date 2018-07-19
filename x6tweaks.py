5d4c638ebb479385aa1cddc4de1d70cf52379f91
x6fan <x6fan@airmail.cc>
2018-07-19 15:57:09 -0300

    Create compute_sha1 function

--- x6tweaks.py
+++ x6tweaks.py
@@ -1,24 +1,26 @@
 #! /usr/bin/env python
 # Mega Man X6 tweaks
 
+# Computes SHA-1 of a readable stream
+def compute_sha1(io, block_size = 64 * 1024):
+    from hashlib import sha1
+
+    sha1_hash = sha1()
+    blocks = iter(lambda: io.read(block_size), b'')
+
+    for block in blocks:
+        sha1_hash.update(block)
+
+    return sha1_hash.hexdigest()
+
 # Image:  Mega Man X6 (USA) (v1.1).bin
 # Size:   599985792 bytes
 # CRC-32: 8e6d014d
 # MD5:    237b6feddd1a88e86ab1cddc8822f03f
 # SHA-1:  d4f7e08371027a87a3bf13311db5a4c56733f4ea
 # Redump: http://redump.org/disc/32516/
-def verify(x6_image, target_sha1 = 'd4f7e08371027a87a3bf13311db5a4c56733f4ea', block_size = 64 * 1024):
-    from hashlib import sha1
-
-    sha1_hash = sha1()
-    blocks = iter(lambda: x6_image.read(block_size), b'')
-
-    for block in blocks:
-        sha1_hash.update(block)
-
-    computed_sha1 = sha1_hash.hexdigest()
-
-    return computed_sha1 == target_sha1
+def verify(x6_image, target_sha1 = 'd4f7e08371027a87a3bf13311db5a4c56733f4ea'):
+    return compute_sha1(x6_image) == target_sha1
 
 def main(path):
     print('Verifying Mega Man X6 image: %s' % path)
